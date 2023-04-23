5f3e95dbb0184dd45263ed95a3be9e6750c849e5
Enital <117684114+Enital@users.noreply.github.com>
2023-04-23 17:19:10 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import { PixabayAPI } from "./js/PixabayAPI";
+import { Notify } from "notiflix";
+import simpleLightbox from "simplelightbox";
 
 const api = new PixabayAPI;
-
+// const refs = {
+// card:
+    
+// }
 const inputFormDataEl = document.querySelector('#search-form');
+const loadMoreBtnEl = document.querySelector('.load-more')
 
 inputFormDataEl.addEventListener('submit', loadImage);
-const photoData = 'cat';
-function loadImage(event) {
+loadMoreBtnEl.addEventListener('click', loadMoreFunc);
+
+// const photoData = 'cat';
+function loadImage(event) {    
     event.preventDefault();
-    api.getImages(event);
+    console.log(inputFormDataEl);
+    inputData = inputFormDataEl.searchQuery.value.trim();
+    // console.log(inputData);
+    api.getImages(inputData);
 }
 
+async function loadMoreFunc() {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -2,26 +2,32 @@ import axios from 'axios';
 
 export class PixabayAPI {
     #BASE_URL = 'https://pixabay.com/api/';
-    #API_KEY = '35660997-4fd052661528ba3040eb8e5ad';    
+    #API_KEY = '35660997-4fd052661528ba3040eb8e5ad';
+    #query = '';
 
-    async getImages(data) {
-        console.log('enter');
-        const url = `${this.#BASE_URL}?key=${this.#API_KEY}&q=${data}&image_type=photo&orientation=horizontal&safesearch=true`;
+    constructor() {
+        this.perPage = 40;
+        currentPage = 1;
+    }
+
+    async getImages(query) {
+        // console.log('enter qetimage');
+        const url = `${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.#query}&image_type=photo&orientation=horizontal&safesearch=true`;
     try {
         const response = await axios.get(url);
-        console.log(response.hits);
-        return response;
+        console.log(response.data);
+        return response?.data;
         }
     catch(error) {
             console.log(error);
         }
     }
-}
 
-// webformatURL - посилання на маленьке зображення для списку карток.
-// largeImageURL - посилання на велике зображення.
-// tags - рядок з описом зображення. Підійде для атрибуту alt.
-// likes - кількість лайків.
-// views - кількість переглядів.
-// comments - кількість коментарів.
-// downloads - кількість завантажень.
+    updatePage() {
+        return this.currentPage += 1;
+    }
+
+    set query(newQuery) {
+        this.#query = newQuery;
+    }
+}
